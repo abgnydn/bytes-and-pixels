3400394b5d67b5620f9d68cf615b0d47e015b62e
Ahmet Barış Günaydın <45149948+abgnydn@users.noreply.github.com>
2023-04-25 11:15:14 +0300

    feat: react-query basic configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const router = createBrowserRouter([]);
+
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools position="bottom-right" />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
