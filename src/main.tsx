6445b4433ab750a983d1026b54e187441deb91e2
Ahmet Barış Günaydın <45149948+abgnydn@users.noreply.github.com>
2023-04-25 13:56:46 +0300

    feat: create index route

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    index: true,
+    element: <Root />,
+  },
+]);
 
 const queryClient = new QueryClient();
 
