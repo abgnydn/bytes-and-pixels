d36187613505f0521ff9b6e7581a3f55a45608ef
Ahmet Barış Günaydın <45149948+abgnydn@users.noreply.github.com>
2023-04-25 11:08:48 +0300

    feat: react router basic configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([]);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById("root")
 );
