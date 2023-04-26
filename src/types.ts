6a6952679fa5c5b04014c038ac5823587a82c036
Ahmet Barış Günaydın <45149948+abgnydn@users.noreply.github.com>
2023-04-26 15:27:45 +0300

    refactor: import all types from types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,58 @@
+//User
 export interface User {
   id?: string | null;
   firstName: string;
   lastName: string;
+}
+
+//Button
+export type ButtonVariant = "add" | "delete" | "edit" | "back" | "yes" | "no";
+
+export interface ButtonProps {
+  variant?: ButtonVariant;
+  children?: React.ReactNode;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+  buttonRef?:
+    | ((instance: HTMLButtonElement | null) => void)
+    | React.RefObject<HTMLButtonElement>
+    | null
+    | undefined;
+}
+
+//Confirmation
+
+export interface ConfirmationModalProps {
+  isOpen: boolean;
+  onConfirm: () => void;
+  onCancel: () => void;
+  message: string;
+  buttonRef?:
+    | ((instance: HTMLButtonElement | null) => void)
+    | React.RefObject<HTMLButtonElement>
+    | null
+    | undefined;
+}
+
+//Table
+export interface TableProps {
+  data: User[];
+}
+export interface Actions {
+  value: User | null;
+  action: string;
+  message: string;
+}
+export type ButtonRef =
+  | ((instance: HTMLButtonElement | null) => void)
+  | React.RefObject<HTMLButtonElement>
+  | null
+  | undefined;
+
+//Input
+export interface TextInputProps {
+  label: string;
+  name: string;
+  type: string;
+  placeholder: string;
 }
