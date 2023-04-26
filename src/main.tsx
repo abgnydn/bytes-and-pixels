2b1ba25ee84adb2b19a49b950dc47edfbe8df9fe
Ahmet Barış Günaydın <45149948+abgnydn@users.noreply.github.com>
2023-04-26 05:44:31 +0300

    feat: import detail page and toast container

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+// Import necessary dependencies and components
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -5,12 +6,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Details from "./routes/details";
+import { ToastContainer } from "react-toastify";
 
 const router = createBrowserRouter([
   {
     path: "/",
     index: true,
     element: <Root />,
+  },
+  {
+    path: "/details/:userId",
+    index: true,
+    element: <Details />,
   },
 ]);
 
@@ -19,6 +27,18 @@ const queryClient = new QueryClient();
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <RouterProvider router={router} />
       <ReactQueryDevtools position="bottom-right" />
     </QueryClientProvider>
